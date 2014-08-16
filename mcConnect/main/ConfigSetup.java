c32233ae2d2e178bfece3a930c2cb64d113018d4
Ylvakiller <rem.geuze@gmail.com>
2014-08-16 23:52:11 +0200

    More classes

---
+++ mcConnect/main/ConfigSetup.java
@@ -0,0 +1,5 @@
+package main;
+
+public class ConfigSetup {
+// TODO code for setting all the settings read from a config file by FileReader
+}
---
+++ mcConnect/main/FileReader.java
@@ -0,0 +1,5 @@
+package main;
+
+public class FileReader {
+// TODO create a general file reader to be used for the config reader class
+}
