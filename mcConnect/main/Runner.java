20f7c4749c80bce4b47601a485999b70f03677e0
Ylvakiller <rem.geuze@gmail.com>
2014-08-16 23:38:21 +0200

    changed the main package

--- mcConnect/main/Runner.java
+++ mcConnect/main/Runner.java
@@ -1,11 +1,11 @@
-package starter;
+package main;
 
 public class Runner {
 
 	public static void main(String[] args) {
 		// TODO initialisation
 		// TODO correctely starting all classes
-
+		// TODO code reader for config file
 	}
 
 }
